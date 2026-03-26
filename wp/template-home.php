e52aba9d76048b107ba63ad4f80b09a4ea3b2775
Fabio <fabiochdev@gmail.com>
2026-03-26 11:07:04 -0400

    web todo postres 1.0.98.1

---
+++ wp/template-home.php
@@ -0,0 +1,11 @@
+<?php
+/* Template: Home */
+?>
+
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head(); ?>
+    <link rel="stylesheet" href="<?php 
