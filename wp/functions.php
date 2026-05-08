5ddafb9f2057b541b086a427f550eb3322424bd8
Fabio <fabiochdev@gmail.com>
2026-05-08 17:26:04 -0400

    web todo postres 2.9.45

--- wp/functions.php
+++ wp/functions.php
@@ -28,32 +28,32 @@ add_action('wp_enqueue_scripts', 'your_theme_enqueue_styles');
 /*  Add your own functions below this line.
     ======================================== */ 
 // Debug: Listar todas las plantillas detectadas
-add_action('admin_footer', function() {
-  if (current_user_can('manage_options')) {
-    $templates = wp_get_theme()->get_page_templates();
-    echo '<pre style="background:#f00; color:#fff; position:fixed; bottom:0; right:0; z-index:9999;">';
-    echo "Plantillas detectadas:\n";
-    print_r($templates);
-    echo '</pre>';
-  }
-});
+//add_action('admin_footer', function() {
+  //if (current_user_can('manage_options')) {
+    //$templates = wp_get_theme()->get_page_templates();
+    //echo '<pre style="background:#f00; color:#fff; position:fixed; bottom:0; right:0; z-index:9999;">';
+    //echo "Plantillas detectadas:\n";
+    //print_r($templates);
+    //echo '</pre>';
+  //}
+//});
 
 /**
  * Registrar mis plantillas personalizadas manualmente
  */
-function registrar_mis_plantillas($templates) {
+//function registrar_mis_plantillas($templates) {
   // Array con tus plantillas: 'archivo.php' => 'Nombre visible'
-  $mis_plantillas = array(
-    'template-home.php'      => 'Home',
+  //$mis_plantillas = array(
+    //'template-home.php'      => 'Home',
     // 'template-landing.php'      => 'Landing de Ventas',
     // 'template-herramienta.php'  => 'Calculadora Herramienta',
     // 'template-contacto.php'     => 'Página de Contacto Especial',
     // 'template-blog-custom.php'  => 'Blog Personalizado',
     // 'template-sin-header.php'   => 'Página Sin Header/Footer',
-  );
+  //);
     
   // Combinar con las plantillas existentes
-  return array_merge($templates, $mis_plantillas);
-}
-add_filter('theme_page_templates', 'registrar_mis_plantillas', 999);
+  //return array_merge($templates, $mis_plantillas);
+//}
+//add_filter('theme_page_templates', 'registrar_mis_plantillas', 999);
 
